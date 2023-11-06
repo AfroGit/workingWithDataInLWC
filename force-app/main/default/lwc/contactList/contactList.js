12ac5b84bab901557efe802166967d7003a889ac
Maurice Mulili <c0decampzulu@gmail.com>
2023-11-06 17:01:43 -0500

    use the reduceErrors function to handle errors

--- force-app/main/default/lwc/contactList/contactList.js
+++ force-app/main/default/lwc/contactList/contactList.js
@@ -1,8 +1,11 @@
 import { LightningElement, wire } from 'lwc';
+
+
 import FIRSTNAME_FIELD from '@salesforce/schema/Contact.FirstName';
 import LASTNAME_FIELD from '@salesforce/schema/Contact.LastName';
 import EMAIL_FIELD from '@salesforce/schema/Contact.Email';
 import getContacts from '@salesforce/apex/ContactController.getContacts';
+import { reduceErrors } from 'c/ldsUtils';
 
 const COLUMNS = [
     { label: 'FirstName', fieldName: FIRSTNAME_FIELD.fieldApiName, type: 'text' },
@@ -13,4 +16,10 @@ export default class AccountList extends LightningElement {
     columns = COLUMNS;
     @wire(getContacts)
     contacts;
+
+    get errors() {
+        return (this.contacts.error) ?
+            reduceErrors(this.contacts.error) : [];
+    }
+
 }
